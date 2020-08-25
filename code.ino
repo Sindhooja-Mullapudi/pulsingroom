3a6e53cedbb1292de51b9b9168103e1498c9f5b6
Sindhooja-Mullapudi <32381685+Sindhooja-Mullapudi@users.noreply.github.com>
2020-08-24 22:53:50 -0700

    Update code.ino

--- code.ino
+++ code.ino
@@ -31,3 +31,40 @@ void setup() {
  pulseSensor.setSerial(Serial);
  pulseSensor.setOutputType(OUTPUT_TYPE);
  pulseSensor.setThreshold(THRESHOLD);
+ // Now that everything is ready, start reading the PulseSensor signal.
+ if (!pulseSensor.begin()) {
+ /*
+ PulseSensor initialization failed,
+ likely because our particular Arduino platform interrupts
+ aren't supported yet.
+ If your Sketch hangs here, try changing USE_ARDUINO_INTERRUPTS to false.
+ which doesn't use interrupts.
+ */
+ for(;;) {
+ // Flash the led to show things didn't work.
+ digitalWrite(PULSE_BLINK, LOW);
+ delay(50);
+ digitalWrite(PULSE_BLINK, HIGH);
+ delay(50);
+ }
+ }
+}
+void loop() {
+ delay(20);
+ pulseSensor.outputSample();
+ /*
+ If a beat has happened since we last checked,
+ write the per-beat information to Serial.
+ write a frequency to the PIN_SPEAKER
+ */
+ if (pulseSensor.sawStartOfBeat()) {
+ pulseSensor.outputBeat();
+ tone(PIN_SPEAKER,7000);
+ }
+ /*
+ The Pulse variable is true only for a short time after the heartbeat is detected
+ */
+ if(pulseSensor.isInsideBeat() == false){
+ noTone(PIN_SPEAKER);
+ }
+}
